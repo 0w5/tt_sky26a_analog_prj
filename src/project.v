384ef961032e94427a0c4ac1a8708c0f276dc6df
しもみ <stellarmic@gmail.com>
2026-04-16 10:55:39 +0900

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_0w5_analog (
+module tt_um_shimomi_analog (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
     input  wire       VAPWR,    // 3.3v power supply
